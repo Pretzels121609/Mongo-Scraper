b2cb9ea6510c4acad5ea601a09451d3e3e0d5ae5
Kathleen E. Giebelhausen <KathleenEGiebelhausen@Kathleens-MacBook-Pro.local>
2018-06-04 18:33:26 -0700

    setting up files

---
+++ server.js
@@ -0,0 +1,10 @@
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var cheerio = require("cheerio");
+var request = require("request");
+
+// I'm not sure what this is for or if I need it
+// var logger = require("morgan");
